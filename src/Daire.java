fb1f988b2a621f128066ee116ef320bf073d5ea2
Dilarahazir1 <37443427+Dilarahazir1@users.noreply.github.com>
2020-05-20 18:31:00 +0300

    Add files via upload

---
+++ src/Daire.java
@@ -0,0 +1,18 @@
+public class Daire extends Hesaplama{
+private double yaricap;
+    public Daire(double yaricap) {
+        this.yaricap = yaricap;
+    }
+    @Override
+    double alan() {
+    return Math.PI*(yaricap*yaricap);   
+    }
+
+    public double getYaricap() {
+        return yaricap;
+    }
+
+    public void setYaricap(double yaricap) {
+        this.yaricap = yaricap;
+    }
+}
